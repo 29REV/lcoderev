16dd348657656b148c1faa7ef4e0270c0a9d8e03
Srirevanth A <203122134+29REV@users.noreply.github.com>
2026-07-08 09:14:31 +0530

    Create 37-SudokuSolver

---
+++ 37-SudokuSolver/37-SudokuSolver.java
@@ -0,0 +1,55 @@
+// Last updated: 7/8/2026, 9:14:29 AM
+class Solution {
+
+    public void solveSudoku(char[][] board) {
+        backtrack(board);
+    }
+
+    private boolean backtrack(char[][] board) {
+
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+
+                if (board[row][col] == '.') {
+
+                    for (char num = '1'; num <= '9'; num++) {
+
+                        if (isValid(board, row, col, num)) {
+
+                            board[row][col] = num;
+
+                            if (backtrack(board))
+                                return true;
+
+                            board[row][col] = '.';
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char num) {
+
+        for (int i = 0; i < 9; i++) {
+
+            if (board[row][i] == num)
+                return false;
+
+            if (board[i][col] == num)
+                return false;
+
+            int r = 3 * (row / 3) + i / 3;
+            int c = 3 * (col / 3) + i % 3;
+
+            if (board[r][c] == num)
+                return false;
+        }
+
+        return true;
+    }
+}
